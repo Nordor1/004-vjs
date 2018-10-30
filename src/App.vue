831e4b110db65e8bd8c4e01f69edac630c043a28
Nordor1 <feliped3carvalho@gmail.com>
2018-10-30 00:05:32 -0300

    components, views, main

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="app">
+    <div id="brand">
+      <router-link to="/">Home</router-link> 
+    </div>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/produtos">Produtos</router-link> 
+      <router-link to="/contato">Contato</router-link> 
+      <router-link to="/about">Sobre</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+body{
+    background: #ebebeb;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,6 +26,20 @@
 #nav {
   padding: 30px;
   a {
+    font-weight: bold;
+    color: #2c3e50;
+    margin: 15px;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+#brand{
+  padding: 30px;
+  float: left;
+  margin-left: 130px;
+  a {
+    // font-size: 20px;
     font-weight: bold;
     color: #2c3e50;
     &.router-link-exact-active {
---
+++ src/views/contato.vue
@@ -0,0 +1,15 @@
+<template>
+    <ContactForm/>
+</template>
+
+<script>
+// @ is an alias to /src
+import ContactForm from "@/components/Forms/ContactForm.vue";
+
+export default {
+  name: "contato",
+  components: {
+    ContactForm
+  }
+};
+</script>
